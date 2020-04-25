fcd4f69ef49e1f902464b0fbdea78e37547fe41e
aderesie <aderesie.m.s@gmail.com>
2020-04-25 22:23:55 +0700

    Home Fragment ( Calculating )

--- app/src/main/java/lab/chevalier/bmi/HomeFragment.kt
+++ app/src/main/java/lab/chevalier/bmi/HomeFragment.kt
@@ -1,22 +1,160 @@
 package lab.chevalier.bmi
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import lab.chevalier.bmi.databinding.FragmentHomeBinding
+import java.text.DecimalFormat
+import kotlin.math.pow
 
 /**
  * A simple [Fragment] subclass.
  */
+
+@Suppress("DEPRECATION")
+@SuppressLint("SetTextI18n")
 class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
+    private var tinggiBadan = 0.0
+    private var beratBadan = 0.0
+    private var bmi = 0.0
+    private val df = DecimalFormat("#.##")
+    private var kategori = 0
+    private var kelamin = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        setHasOptionsMenu(true)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = requireActivity().findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.btnHitung.setOnClickListener { check() }
+    }
+
+    private fun check(){
+        when {
+            binding.edBeratBadan.text.isEmpty() || binding.edTinggiBadan.text.isEmpty() || binding.rgKelamin.checkedRadioButtonId == -1 -> {
+                Toast.makeText( activity, "Isian tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            }
+            else -> hitung()
+        }
+    }
+
+    private fun hitung(){
+
+        tinggiBadan = binding.edTinggiBadan.text.toString().toDouble() / 100
+        beratBadan = binding.edBeratBadan.text.toString().toDouble()
+
+        bmi = beratBadan / tinggiBadan.pow(2)
+
+        binding.tvBmi.text = "Nilai BMI = ${df.format(bmi)}"
+
+        setVisible()
+
+        if (binding.rgKelamin.checkedRadioButtonId == R.id.rb_pria){
+            kelamin = 1
+            if (bmi < 20.50){
+                setKurus()
+            } else if (bmi >= 20.50 && bmi < 27.0){
+                setIdeal()
+            } else {
+                setGemuk()
+            }
+
+        } else {
+            kelamin = 2
+            if (bmi < 18.50){
+                setKurus()
+            } else if (bmi >=18.50 && bmi < 25.0){
+                setIdeal()
+            } else {
+                setGemuk()
+            }
+        }
+    }
+
+    private fun setVisible(){
+        binding.tvBmi.visibility = View.VISIBLE
+        binding.tvKategori.visibility = View.VISIBLE
+        binding.btnSaran.visibility = View.VISIBLE
+        binding.btnShare.visibility = View.VISIBLE
+    }
+
+    private fun setKurus(){
+        binding.tvKategori.text = "Kurus"
+        kategori = 1
+        binding.tvKategori.setTextColor(requireActivity().resources.getColor(R.color.kategori_kurus))
+    }
+    private fun setIdeal(){
+        binding.tvKategori.text = "Ideal"
+        kategori = 2
+        binding.tvKategori.setTextColor(requireActivity().resources.getColor(R.color.kategori_ideal))
+    }
+    private fun setGemuk(){
+        binding.tvKategori.text = "Gemuk"
+        kategori = 3
+        binding.tvKategori.setTextColor(requireActivity().resources.getColor(R.color.kategori_gemuk))
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("KATEGORI", kategori)
+        outState.putDouble("BMI", bmi)
+        outState.putInt("KELAMIN", kelamin)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        if (savedInstanceState != null && savedInstanceState.getDouble("BMI") != 0.0){
+            bmi = savedInstanceState.getDouble("BMI")
+            kategori = savedInstanceState.getInt("KATEGORI")
+            kelamin = savedInstanceState.getInt("KELAMIN")
+
+            binding.tvBmi.text = "Nilai BMI = ${df.format(bmi)}"
+
+            setVisible()
+
+            if (kelamin == 1){
+                if (bmi < 20.50){
+                    setKurus()
+                } else if (bmi >= 20.50 && bmi < 27.0){
+                    setIdeal()
+                } else {
+                    setGemuk()
+                }
+            } else {
+                if (bmi < 18.50){
+                    setKurus()
+                } else if (bmi >=18.50 && bmi < 25.0){
+                    setIdeal()
+                } else {
+                    setGemuk()
+                }
+            }
+        }
     }
 
 }
